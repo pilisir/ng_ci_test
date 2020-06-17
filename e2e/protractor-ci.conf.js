74620e53d2b3179827edd528d02cb51de425ebd2
jasonjian <jasonjian@flowring.com>
2020-06-17 23:10:55 +0800

    update

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -6,15 +6,16 @@ config.capabilities = {
   chromeOptions: {
     args: ["--headless", "--no-sandbox"],
   },
-  onPrepare() {
-    require("ts-node").register({
-      project: require("path").join(__dirname, "./tsconfig.json"),
-    });
+};
 
-    jasmine
-      .getEnv()
-      .addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  },
+config.onPrepare() = () => {
+  require("ts-node").register({
+    project: require("path").join(__dirname, "./tsconfig.json"),
+  });
+
+  jasmine
+    .getEnv()
+    .addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
 };
 
 exports.config = config;
