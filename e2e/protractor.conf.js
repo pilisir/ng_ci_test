d4599592e1dcfcdcf8da4e9c778c36c1ce32031b
jasonjian <jasonjian@flowring.com>
2020-06-17 22:16:11 +0800

    update

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -2,8 +2,8 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-// const { SpecReporter } = require("jasmine-spec-reporter");
-var HtmlReporter = require("protractor-beautiful-reporter");
+const { SpecReporter } = require("jasmine-spec-reporter");
+// var HtmlReporter = require("protractor-beautiful-reporter");
 
 /**
  * @type { import("protractor").Config }
@@ -27,29 +27,29 @@ exports.config = {
       project: require("path").join(__dirname, "./tsconfig.json"),
     });
 
-    var m = new Date();
-    var dateString =
-      m.getUTCFullYear() +
-      "" +
-      ("0" + (m.getUTCMonth() + 1)).slice(-2) +
-      "" +
-      ("0" + m.getUTCDate()).slice(-2) +
-      "" +
-      ("0" + m.getUTCHours()).slice(-2) +
-      "" +
-      ("0" + m.getUTCMinutes()).slice(-2) +
-      "" +
-      ("0" + m.getUTCSeconds()).slice(-2);
+    // var m = new Date();
+    // var dateString =
+    //   m.getUTCFullYear() +
+    //   "" +
+    //   ("0" + (m.getUTCMonth() + 1)).slice(-2) +
+    //   "" +
+    //   ("0" + m.getUTCDate()).slice(-2) +
+    //   "" +
+    //   ("0" + m.getUTCHours()).slice(-2) +
+    //   "" +
+    //   ("0" + m.getUTCMinutes()).slice(-2) +
+    //   "" +
+    //   ("0" + m.getUTCSeconds()).slice(-2);
 
     jasmine
       .getEnv()
-      // .addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-      .addReporter(
-        new HtmlReporter({
-          baseDirectory: "e2e/result/screenshots/" + dateString,
-          docTitle: "Angular " + new Date().toLocaleString(),
-          preserveDirectory: false,
-        }).getJasmine2Reporter()
-      );
+      .addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    // .addReporter(
+    //   new HtmlReporter({
+    //     baseDirectory: "e2e/result/screenshots/" + dateString,
+    //     docTitle: "Angular " + new Date().toLocaleString(),
+    //     preserveDirectory: false,
+    //   }).getJasmine2Reporter()
+    // );
   },
 };
